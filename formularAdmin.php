00ad3d3126baffbe8eabcf628804cdd0928d42eb
Mqroman <48622213+Mqroman@users.noreply.github.com>
2019-06-09 17:14:09 +0300

    Add files via upload

---
+++ formularAdmin.php
@@ -0,0 +1,226 @@
+<!DOCTYPE HTML>
+<apex:page  applyHtmlTag="false">
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>imprumuturi</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	
+</head>
+<body>
+	<div id="header">
+		<div>
+		<?php
+			if($_COOKIE['is_logged']==="LOGGED"){
+				echo "<form action=".'"'."logout_process.php".'"'." method=".'"'."POST".'"'." >";	
+				echo "<button type=".'"'."submit".'"'."name=".'"'."logbutton".'"'.">LOGOUT</button>";
+				
+				echo "</form>";
+			}
+			?>
+			<a href="index.php" class="logo"><img src="images/logo.png" alt=""></a>
+			<form action="index.php">
+				
+
+			</form>
+		</div>
+		<div class="navigation">
+			<ul>
+				<li>
+					<a href="index.php">home</a>
+				</li>
+				
+				</li>
+				<li class="selected">
+					<a href="services.php">services</a>
+					<ul>
+						<li>
+							<a href="serv/revizii.php">Revizii</a>
+						</li>
+						<li>
+							<a href="serv/cadru.php">Cadru</a>
+						</li>
+						<li>
+							<a href="serv/furca.php">Furca si shock</a>
+						</li>
+						<li>
+							<a href="serv/roti.php">Roti</a>
+						</li>
+						<li>
+							<a href="serv/frane.php">Frane</a>
+						</li>
+						<li>
+							<a href="serv/transmisie.php">Transmisie</a>
+						</li>
+						<li>
+							<a href="serv/alte.php">Altele</a>
+						</li>
+						
+					</ul>
+				</li>
+				<li>
+					<a href="contact.php">comenzi</a>
+				</li>
+				<li class="booking">
+					<a href="booking.php">Calendar</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="body">
+		<div class="content">
+			<div class="section">
+				<div class="contact">
+					
+				<h1 class="avatar">Formular Administrator</h1>
+
+   
+        <form class="continut" action="booking.php">
+            <div class="imgcontainer">
+               
+
+            </div>
+
+            <div class="container">
+
+               
+                <br />
+
+				<br />
+				<label for="oname"><b>Object Name</b></label>
+				<input type="text" placeholder="Enter Object Name" id="object" required>
+				<br />
+
+				<br />
+				<label for="onr"><b>Object Nr.</b></label>
+                <input type="number" placeholder="Enter Number" id="price" required>
+
+                <br />
+                <label for="detalii"><b>Details</b></label>
+                <br />
+
+                <textarea rows="4" cols="50" class="detalii" placeholder="Enter Details" id="detalii">
+                </textarea>
+
+
+               
+				<br />
+                <br />
+                <label for="address"><b>Address</b></label>
+                <br />
+                <input type="text" placeholder="Enter Address" id="address" required>
+				<br />
+                <br />
+
+                <button type="submit">Submit</button>
+
+
+            </div>
+
+
+
+        </form>
+					
+				</div>
+			</div>
+			<div class="sidebar">
+				<div class="contact">
+					<div>
+						<a ><img src="images/lava2.jpg" alt=""></a>
+					</div>
+					
+							<ul>
+								<li>
+									<b>address:</b> <span>426 strada repar</span>
+								</li>
+								<li>
+									<b>phone:</b> <span>353737353</span>
+								</li>
+								<li>
+									<b>fax:</b> <span>373753735533</span>
+								</li>
+								<li>
+									<b>email:</b> <span><a href="">bicicleteSRL@yahoo.com</a></span>
+								</li>
+							</ul>
+						
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+			<div>
+				<div class="contact">
+					<h3>contact information</h3>
+					<ul>
+						<li>
+							<b>address:</b> <span>426 strada repar</span>
+						</li>
+						<li>
+							<b>phone:</b> <span>04433344433</span>
+						</li>
+						<li>
+							<b>fax:</b> <span>43322324432</span>
+						</li>
+						<li>
+							<b>email:</b> <span><a href="">biciclete@yahoo.com</a></span>
+						</li>
+					</ul>
+				</div>
+				<div class="tweets">
+					<h3>recent tweets</h3>
+					<ul>
+						<li>
+							<a href="#">blablalbalbalalvalvalvlablalbalblalblablab.<span>1 day ago</span></a>
+						</li>
+						<li>
+							<a href="#">blablalbalbla.<span>2 days ago</span></a>
+						</li>
+					</ul>
+				</div>
+				<div class="posts">
+					<h3>recent blog post</h3>
+					<ul>
+						<li>
+							<a href="#">roti noi</a>
+						</li>
+						<li>
+							<a href="#">repar tot</a>
+						</li>
+						<li>
+							<a href="#">stoc epuizat</a>
+						</li>
+						
+					</ul>
+				</div>
+				<div class="connect">
+					<h3>stay in touch</h3>
+					<p>
+						pentru detalii
+					</p>
+					<ul>
+						<li id="facebook">
+							<a href="">facebook</a>
+						</li>
+						<li id="twitter">
+							<a href="">twitter</a>
+						</li>
+						<li id="googleplus">
+							<a href="">googleplus</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="section">
+				<p>
+					&copy; uaic iasi
+				</p>
+				
+			</div>
+		</div>
+	</body>
+	<?php 
+					
+						?>
+	</html>
