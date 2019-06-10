88a7ece6feaa650fc125481374a3b101a2bcb104
Mihneass <48487418+Mihneass@users.noreply.github.com>
2019-06-10 13:58:50 +0300

    Add files via upload

---
+++ tw/process_delete_client.php
@@ -0,0 +1,39 @@
+<?php
+$conn = oci_connect("student", "student", "localhost:1521/xe");
+if (!$conn) {
+  $m = oci_error();
+   echo $m['message'], "\n";
+   exit;
+			}
+$enquiry="DELETE FROM CERERI WHERE IDK=:iddd";
+
+$stid = oci_parse($conn, $enquiry);
+oci_bind_by_name($stid, ":iddd", $_POST['request-id']);
+echo $_POST['request-id'];
+
+oci_execute($stid);
+$thisUser=array();
+    $fileContent = file_get_contents('./jsons/requests.json');
+    $thisUser=json_encode($thisUser);
+    file_put_contents('./jsons/requests.json',$thisUser);
+                $enquiry="SELECT * FROM cereri";
+                $stid = oci_parse($conn, $enquiry);
+                oci_execute($stid);	
+                      while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)){
+                          $thisReq=array(
+                              'id'=>$row['IDK'],
+                              'id_user'=>$row['ID_USER'],
+                              'situation'=>$row['SITUATIE'],
+                              'obj_name'=>$row['NUME_OBIECT'],
+                              'obj_nr'=>$row['NR_OBIECTE'],
+                              'address'=>$row['ADDRESS']
+                            );
+                            $fileContent = file_get_contents('./jsons/requests.json');
+                            $information = json_decode($fileContent); 
+                          array_push($information,$thisReq);
+                            $formatedInfo=json_encode($information);
+                            file_put_contents('./jsons/requests.json',$formatedInfo);
+                          }
+header('Location: contact.php');
+
+?>
